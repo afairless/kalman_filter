37cf5820d02bddbcea526df590e330ac25f23d10
gv <afairless@gmail.com>
2024-05-08 10:41:55 -0400

    notebook draft complete

--- notebooks/kalman.py
+++ notebooks/kalman.py
@@ -1,4 +1,11 @@
-# # Introduction
+# # State-Space Models:  Learning the Kalman Filter
+
+# ## Translating Equations
+
+# +
+from pathlib import Path
+from IPython.display import Image
+# -
 
 # + [markdown]
 '''
@@ -27,9 +34,10 @@ deeper into the basics of state-space models, which inevitably led me to
 [Kalman filters](https://en.wikipedia.org/wiki/Kalman_filter).  However, I found
 various sources confusing because they used different notation in specifying the
 filter's equations.  The [control](https://en.wikipedia.org/wiki/Control_system) 
-research literature uses one set of variable names, while the statistical time
-series literature uses another set.  To reconcile these for my own 
-understanding, I created a table of correspondences.  My two main sources were:
+research literature uses one set of variable names, while the statistical [time
+series](https://en.wikipedia.org/wiki/Time_series) literature uses another set. 
+To reconcile these for my own understanding, I created a table of 
+correspondences.  My two main sources were:
 
 - [Kalman and Bayesian Filters in Python, 2020](
     https://rlabbe.github.io/Kalman-and-Bayesian-Filters-in-Python/)
@@ -86,20 +94,80 @@ Now, the table specifying how the variables correspond:
 | $\mathbf y$ | $$v_t$$ | residual between predicted state and measurement |
 | $\mathbf x$ | $$a_{t|t}$$ | updated state estimate |
 | $\mathbf P$ | $$P_{t|t}$$ | updated state covariance estimate |
-
-In addition to the table above, 
 '''
 # -
 
-# ## header
+# ## Testing Implementation Code
 
-# +
-a = 1
+# + [markdown]
+'''
+In addition to the table above, I coded the Kalman filter in both sets of 
+notation and tested both on several examples of input data, parameters, and 
+results from various sources to verify that the implementation was correct.  The
+two versions in Python/[NumPy](https://numpy.org/) are below:
+'''
 # -
 
 # +
-from pathlib import Path
-from IPython.display import Image
-filepath = Path.cwd() / 'output' / 'filter_state_with_data.png'
+filepath = Path.cwd().parent / 'output' / 'kalman_code.png'
+Image(filename=filepath, width=640)
+# -
+
+# + [markdown]
+'''
+Go [here](
+https://github.com/afairless/kalman_filter/blob/main/src/kalman_filter.py) for 
+the full code.
+
+The [tested examples](
+https://github.com/afairless/kalman_filter/blob/main/tests/test_kalman_filter.py) 
+came from:
+
+- [Kalman Filter For Dummies, 2009](
+    http://bilgin.esme.org/BitsAndBytes/KalmanFilterforDummies)
+    > Bilgin Esme
+
+- [Kalman and Bayesian Filters in Python, 2020](
+    https://rlabbe.github.io/Kalman-and-Bayesian-Filters-in-Python/)
+    > Roger R Labbe Jr
+
+- [Time Series Analysis by State Space Methods, 2nd Edition, 2012](
+    https://www.google.com/books/edition/Time_Series_Analysis_by_State_Space_Meth/lGyshsfkLrIC)
+    > J. Durbin, S.J. Koopman
+
+The [Durbin and Koopman (2012)](
+https://www.google.com/books/edition/Time_Series_Analysis_by_State_Space_Meth/lGyshsfkLrIC) 
+example using the classic Nile River data set didn't provide exact numbers for 
+its results, but the results do appear in plots in Figure 2.1 on page 16.  The 
+plots that I obtained appear to match their plots very closely.
+
+First, the data points and the filtered state shown in Figure 2.1 (i):
+'''
+# -
+
+# +
+filepath = Path.cwd().parent / 'output' / 'filter_state_with_data.png'
+Image(filename=filepath, width=640)
+# -
+
+# + [markdown]
+'''
+Second, the filter variance shown in Figure 2.1 (ii):
+'''
+# -
+
+# +
+filepath = Path.cwd().parent / 'output' / 'filter_variance.png'
+Image(filename=filepath, width=640)
+# -
+
+# + [markdown]
+'''
+Finally, the residuals shown in Figure 2.1 (iii):
+'''
+# -
+
+# +
+filepath = Path.cwd().parent / 'output' / 'prediction_errors.png'
 Image(filename=filepath, width=640)
 # -
